62b7aa1861374c1e3484a6f3f225cbee846a1e82
Connor <connormakhlouta@gmail.com>
2016-06-09 19:22:19 +0300

    First commit

---
+++ Dome.js
@@ -0,0 +1,16 @@
+/**
+ * Created by connormakhlouta on 6/9/16.
+ */
+window.dome = (function () {
+    function Dome (els) {
+
+    }
+
+    var dome = {
+        get: function (selector) {
+
+        }
+    };
+
+    return dome;
+}());
